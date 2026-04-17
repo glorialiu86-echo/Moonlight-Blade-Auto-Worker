607f5e75b4c0c19900f1738b6c723001f688f66a
glorialiu86-echo <glorialiu86@gmail.com>
2026-04-17 15:14:12 +0800

    Add Windows resident server launcher

---
+++ scripts/run-server-forever.cmd
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set "ROOT=C:\Users\ZMT-User\Downloads\天刀控制系统"
+set "LOG_DIR=%ROOT%\tmp\service-logs"
+set "NODE_EXE=C:\Program Files\nodejs\node.exe"
+
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+
+cd /d "%ROOT%"
+
+:restart
+echo [%date% %time%] starting server >> "%LOG_DIR%\server.log"
+"%NODE_EXE%" src\server\index.js >> "%LOG_DIR%\server.log" 2>&1
+echo [%date% %time%] server exited with code %errorlevel%, restarting in 5 seconds >> "%LOG_DIR%\server.log"
+timeout /t 5 /nobreak >nul
+goto restart
